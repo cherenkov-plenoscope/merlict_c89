25b32671603e477b23cdc0f244590ba188c3d565
Sebastian Achim Mueller <sebmuell@phys.ethz.ch>
2019-04-27 08:59:17 +0200

    have a quadratic-equation solver

---
+++ mliQuadraticEquation.h
@@ -0,0 +1,32 @@
+/* Copyright 2019 Sebastian A. Mueller */
+#ifndef MERLICT_MLIQUADRATICEQUATION_H_
+#define MERLICT_MLIQUADRATICEQUATION_H_
+
+#include <math.h>
+
+int mli_quadratiq_equation(
+    const double p,
+    const double q,
+    double *plus_solution,
+    double *minus_solution) {
+    /*
+        y = a*x^2 + b*x + c
+        p = b/a
+        q = c/a
+        x_m = -p/2 - sqrt((-p/2)^2 - q)
+        x_p = -p/2 + sqrt((-p/2)^2 - q)
+    */
+    const double p_over_2 = 0.5*p;
+    const double inner_part_of_squareroot = p_over_2 * p_over_2 - q;
+    double squareroot;
+    if (inner_part_of_squareroot >= 0.0) {
+        squareroot = sqrt(inner_part_of_squareroot);
+        (*minus_solution) = -p_over_2 - squareroot;
+        (*plus_solution) = -p_over_2 + squareroot;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+#endif
--- test.c
+++ test.c
@@ -1,5 +1,6 @@
 /* Copyright 2019 Sebastian Achim Mueller                                     */
 /* Compile with: gcc test.c -o test -std=c89 -lm -Wall -pedantic              */
+#include <math.h>
 #include "mliVec.h"
 #include "mliUnitTest.h"
 #include "mliMesh.h"
@@ -7,9 +8,77 @@
 #include "mliTriangleIntersection.h"
 #include "mliColor.h"
 #include "mliImage.h"
+#include "mliQuadraticEquation.h"
 
 
 int main(int argc, char *argv[]) {
+    /* mliQuadraticEquation */
+    {
+        /* ax**2 + bx + c = 0 */
+        const double b_over_a = 1.;
+        const double c_over_a = 1.;
+        double plus_solution;
+        double minus_solution;
+        int valid_solutions = mli_quadratiq_equation(
+            b_over_a,
+            c_over_a,
+            &plus_solution,
+            &minus_solution);
+
+        CHECK(!valid_solutions);
+    }
+
+    {
+        /* ax**2 + bx + c = 0 */
+        const double b_over_a = 0.;
+        const double c_over_a = 0.;
+        double plus_solution;
+        double minus_solution;
+        CHECK(mli_quadratiq_equation(
+            b_over_a,
+            c_over_a,
+            &plus_solution,
+            &minus_solution));
+
+        CHECK(minus_solution == 0.);
+        CHECK(plus_solution == 0.);
+    }
+
+    {
+        /* ax**2 + bx + c = 0 */
+        const double b_over_a = 0.;
+        const double c_over_a = -1.;
+        double plus_solution;
+        double minus_solution;
+        CHECK(mli_quadratiq_equation(
+            b_over_a,
+            c_over_a,
+            &plus_solution,
+            &minus_solution));
+
+        CHECK(minus_solution == -1.);
+        CHECK(plus_solution == +1.);
+    }
+
+    {
+        /* ax**2 + bx + c = 0 */
+        double a, b, c;
+        for (a = -5.0; a < 5.0; a = a+.25) {
+            for (b = -5.0; b < 5.0; b = b+.25) {
+                for (c = -5.0; c < 5.0; c = c+.25) {
+                    if (a != 0.0) {
+                        double xp;
+                        double xm;
+                        if (mli_quadratiq_equation(b/a, c/a, &xp, &xm)) {
+                            CHECK_MARGIN(a*xp*xp + b*xp + c, 0., 1e-12);
+                            CHECK_MARGIN(a*xm*xm + b*xm + c, 0., 1e-12);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     /* mliColor */
     {
         mliColor red = {255., 0., 0.};
