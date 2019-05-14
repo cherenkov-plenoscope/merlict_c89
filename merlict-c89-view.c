8d48b41b88de67a72cb7dafca4aa6511fb524c63
Sebastian Achim Mueller <sebmuell@phys.ethz.ch>
2019-05-14 08:40:48 +0200

    unify naming for apps

--- merlict-c89-view.c
+++ merlict-c89-view.c
@@ -1,5 +1,6 @@
 /* Copyright 2019 Sebastian Achim Mueller                                     */
-/* Compile with: gcc mliViewer.c -o view -std=c89 -lm -Wall -pedantic              */
+/* Compile with:                                                              */
+/* gcc merlict-c89-view.c -o merlict-c89-view -std=c89 -lm -Wall -pedantic    */
 #include <math.h>
 #include <stdio.h>
 #include <stdint.h>
