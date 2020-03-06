3eed49a0ee35ec79059132f073945f46df2fc294
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-03-06 11:55:19 +0100

    point like source

--- tests/test_photon_sources.c
+++ tests/test_photon_sources.c
@@ -32,3 +32,34 @@ CASE("parallel_towards_z_from_xy_disc") {
 
         mliDynPhoton_free(&photons);
 }
+
+CASE("point_like_towards_z") {
+        struct mliMT19937 prng = mliMT19937_init(0u);
+        struct mliDynPhoton photons = mliDynPhoton_init();
+        const double opening_angle = mli_deg2rad(1.0);
+        const double wavelength = 433e-9;
+        const uint64_t num_photons = 1000;
+        uint64_t i;
+        CHECK(mliDynPhoton_malloc(&photons, 0));
+
+        CHECK(point_like_towards_z_opening_angle_num_photons(
+                &photons,
+                wavelength,
+                opening_angle,
+                num_photons,
+                &prng));
+
+        for (i = 0; i < photons.dyn.size; i++) {
+                CHECK(photons.arr[i].ray.support.z == 0.0);
+                CHECK(photons.arr[i].ray.support.x == 0.0);
+                CHECK(photons.arr[i].ray.support.y == 0.0);
+                CHECK_MARGIN(
+                        mliVec_norm(photons.arr[i].ray.direction),
+                        1.0,
+                        1e-6);
+                CHECK(photons.arr[i].wavelength == wavelength);
+                CHECK(photons.arr[i].simulation_truth_id == (int64_t)i);
+        }
+
+        mliDynPhoton_free(&photons);
+}
