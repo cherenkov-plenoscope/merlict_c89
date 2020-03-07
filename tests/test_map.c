9e7fd74dfe16b86b0ec614388ac933ac67e41aad
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-03-07 23:18:22 +0100

    put variable length integer in own sources

--- tests/test_map.c
+++ tests/test_map.c
@@ -1,80 +1,5 @@
 /* Copyright 2019-2020 Sebastian Achim Mueller                                */
 
-CASE("dynUint8") {
-        size_t i;
-        struct mliMT19937 prng = mliMT19937_init(0);
-        struct mliDynUint8 a = mliDynUint8_init();
-        struct mliDynUint8 b = mliDynUint8_init();
-        struct mliDynUint8 r = mliDynUint8_init();
-        CHECK(mliDynUint8_malloc(&a, 0));
-        CHECK(mliDynUint8_malloc(&b, 0));
-        CHECK(mliDynUint8_malloc(&r, 0));
-
-        for (i = 0; i<100; i++) {
-                uint64_t aa, bb, rr;
-                aa = (uint64_t)(mli_random_uniform(&prng)*(1000*1000*1000));
-                bb = (uint64_t)(mli_random_uniform(&prng)*(1000*1000*1000));
-
-                CHECK(mli_decimal_to_base255(aa, &a));
-                CHECK(mli_base255_to_decimal(&a) == aa);
-
-                CHECK(mli_decimal_to_base255(bb, &b));
-                CHECK(mli_base255_to_decimal(&b) == bb);
-
-                if (aa > bb) {
-                        CHECK(mliDynUint8_greater_than(&a, &b));
-                } else {
-                        CHECK(!mliDynUint8_greater_than(&a, &b));
-                }
-
-                if (aa == bb) {
-                        CHECK(mliDynUint8_equal(&a, &b));
-                } else {
-                        CHECK(!mliDynUint8_equal(&a, &b));
-                }
-
-                CHECK(mliDynUint8_add(&a, &b, &r));
-                CHECK(r.dyn.size >= MLI_MAX2(a.dyn.size, b.dyn.size));
-                rr = mli_base255_to_decimal(&r);
-                CHECK(aa + bb == rr);
-                if (rr > bb) {
-                        CHECK(mliDynUint8_greater_than(&r, &b));
-                }
-                if (rr > aa) {
-                        CHECK(mliDynUint8_greater_than(&r, &a));
-                }
-
-                CHECK(mliDynUint8_divide_two(&a, &r));
-                rr = mli_base255_to_decimal(&r);
-                CHECK(aa/2 == rr);
-        }
-
-        mliDynUint8_free(&a);
-        mliDynUint8_free(&b);
-        mliDynUint8_free(&r);
-}
-
-CASE("dynUint8_from_string") {
-        size_t i;
-        struct mliDynUint8 a = mliDynUint8_init();
-        char text[6][100] = {
-                "peter",
-                "klaus",
-                "nebert\n",
-                "FoU\t123",
-                "0123",
-                " whitespace ",
-        };
-        CHECK(mliDynUint8_malloc(&a, 0));
-        for (i = 0; i < 6; i++) {
-                CHECK(mliDynUint8_malloc_char(&a, text[i]));
-                CHECK(a.dyn.size == strlen(text[i]) + 1);
-                CHECK(a.arr[strlen(text[i])] == '\0');
-        }
-        mliDynUint8_free(&a);
-}
-
-
 CASE("map") {
         uint64_t ret = 0;
         struct mliMap map = mliMap_init();
@@ -96,6 +21,7 @@ CASE("map") {
 
         CHECK(mliMap_malloc_insert(&map, "karl", 37) == 1);
 
+        CHECK(map.size == 4u);
         mliMap_free(&map);
 }
 
@@ -114,5 +40,11 @@ CASE("map2") {
                 }
         }
 
+        mliMap_free(&map);
+}
+
+CASE("empty_map") {
+        struct mliMap map = mliMap_init();
+        CHECK(map.size == 0u);
         mliMap_free(&map);
 }
