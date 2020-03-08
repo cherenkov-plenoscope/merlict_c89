ab9e57e79fc73e0701d3a4226bd7b2e7542d972f
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-03-08 09:43:07 +0100

    split into individual functions

--- mliScenery_equal.h
+++ mliScenery_equal.h
@@ -4,45 +4,72 @@
 
 #include "mliScenery.h"
 
-int mliScenery_is_equal(const struct mliScenery *a, const struct mliScenery *b)
+int _mliScenery_functions_equal(
+        const struct mliScenery *a,
+        const struct mliScenery *b)
 {
         uint64_t i;
         if (a->num_functions != b->num_functions) return 0;
-        if (a->num_colors != b->num_colors ) return 0;
-        if (a->num_surfaces != b->num_surfaces ) return 0;
-        if (a->num_vertices != b->num_vertices ) return 0;
-        if (a->num_triangles != b->num_triangles ) return 0;
-        if (a->num_spherical_cap_hex != b->num_spherical_cap_hex ) return 0;
-        if (a->num_spheres != b->num_spheres) return 0;
-        if (a->num_cylinders != b->num_cylinders) return 0;
-        if (a->num_hexagons != b->num_hexagons) return 0;
-        if (a->num_bicircleplanes != b->num_bicircleplanes) return 0;
-        if (a->num_discs != b->num_discs) return 0;
-
         for (i = 0; i < a->num_functions; i++) {
                 if (!mliFunc_is_equal(
                         a->functions[i],
                         b->functions[i]))
                         return 0;
         }
+        return 1;
+}
+
+int _mliScenery_colors_equal(
+        const struct mliScenery *a,
+        const struct mliScenery *b)
+{
+        uint64_t i;
+        if (a->num_colors != b->num_colors ) return 0;
         for (i = 0; i < a->num_colors; i++) {
                 if (!mliColor_is_equal(
                         a->colors[i],
                         b->colors[i]))
                         return 0;
         }
+        return 1;
+}
+
+int _mliScenery_surfaces_equal(
+        const struct mliScenery *a,
+        const struct mliScenery *b)
+{
+        uint64_t i;
+        if (a->num_surfaces != b->num_surfaces ) return 0;
         for (i = 0; i < a->num_surfaces; i++) {
                 if (!mliSurface_is_equal(
                         a->surfaces[i],
                         b->surfaces[i]))
                         return 0;
         }
+        return 1;
+}
+
+int _mliScenery_vertices_equal(
+        const struct mliScenery *a,
+        const struct mliScenery *b)
+{
+        uint64_t i;
+        if (a->num_vertices != b->num_vertices ) return 0;
         for (i = 0; i < a->num_vertices; i++) {
                 if (!mliVec_is_equal(
                         a->vertices[i],
                         b->vertices[i]))
                         return 0;
         }
+        return 1;
+}
+
+int _mliScenery_triangles_equal(
+        const struct mliScenery *a,
+        const struct mliScenery *b)
+{
+        uint64_t i;
+        if (a->num_triangles != b->num_triangles ) return 0;
         for (i = 0; i < a->num_triangles; i++) {
                 if (!mliFace_is_equal(
                         a->triangles[i],
@@ -53,6 +80,15 @@ int mliScenery_is_equal(const struct mliScenery *a, const struct mliScenery *b)
                         b->triangles_surfaces[i]))
                         return 0;
         }
+        return 1;
+}
+
+int _mliScenery_spherical_cap_hex_equal(
+        const struct mliScenery *a,
+        const struct mliScenery *b)
+{
+        uint64_t i;
+        if (a->num_spherical_cap_hex != b->num_spherical_cap_hex ) return 0;
         for (i = 0; i < a->num_spherical_cap_hex; i++) {
                 if (!mliSphericalCapHex_is_equal(
                         a->spherical_cap_hex[i],
@@ -67,6 +103,15 @@ int mliScenery_is_equal(const struct mliScenery *a, const struct mliScenery *b)
                         b->spherical_cap_hex_T[i]))
                         return 0;
         }
+        return 1;
+}
+
+int _mliScenery_spheres_equal(
+        const struct mliScenery *a,
+        const struct mliScenery *b)
+{
+        uint64_t i;
+        if (a->num_spheres != b->num_spheres) return 0;
         for (i = 0; i < a->num_spheres; i++) {
                 if (!mliSphere_is_equal(
                         a->spheres[i],
@@ -81,6 +126,15 @@ int mliScenery_is_equal(const struct mliScenery *a, const struct mliScenery *b)
                         b->spheres_T[i]))
                         return 0;
         }
+        return 1;
+}
+
+int _mliScenery_cylinders_equal(
+        const struct mliScenery *a,
+        const struct mliScenery *b)
+{
+        uint64_t i;
+        if (a->num_cylinders != b->num_cylinders) return 0;
         for (i = 0; i < a->num_cylinders; i++) {
                 if (!mliCylinder_is_equal(
                         a->cylinders[i],
@@ -95,6 +149,15 @@ int mliScenery_is_equal(const struct mliScenery *a, const struct mliScenery *b)
                         b->cylinders_T[i]))
                         return 0;
         }
+        return 1;
+}
+
+int _mliScenery_hexagons_equal(
+        const struct mliScenery *a,
+        const struct mliScenery *b)
+{
+        uint64_t i;
+        if (a->num_hexagons != b->num_hexagons) return 0;
         for (i = 0; i < a->num_hexagons; i++) {
                 if (!mliHexagon_is_equal(
                         a->hexagons[i],
@@ -109,6 +172,15 @@ int mliScenery_is_equal(const struct mliScenery *a, const struct mliScenery *b)
                         b->hexagons_T[i]))
                         return 0;
         }
+        return 1;
+}
+
+int _mliScenery_bicircleplanes_equal(
+        const struct mliScenery *a,
+        const struct mliScenery *b)
+{
+        uint64_t i;
+        if (a->num_bicircleplanes != b->num_bicircleplanes) return 0;
         for (i = 0; i < a->num_bicircleplanes; i++) {
                 if (!mliBiCirclePlane_is_equal(
                         a->bicircleplanes[i],
@@ -123,6 +195,15 @@ int mliScenery_is_equal(const struct mliScenery *a, const struct mliScenery *b)
                         b->bicircleplanes_T[i]))
                         return 0;
         }
+        return 1;
+}
+
+int _mliScenery_discs_equal(
+        const struct mliScenery *a,
+        const struct mliScenery *b)
+{
+        uint64_t i;
+        if (a->num_discs != b->num_discs) return 0;
         for (i = 0; i < a->num_discs; i++) {
                 if (!mliDisc_is_equal(
                         a->discs[i],
@@ -137,6 +218,21 @@ int mliScenery_is_equal(const struct mliScenery *a, const struct mliScenery *b)
                         b->discs_T[i]))
                         return 0;
         }
+        return 1;
+}
+
+int mliScenery_is_equal(const struct mliScenery *a, const struct mliScenery *b)
+{
+        if (!_mliScenery_functions_equal(a, b)) return 0;
+        if (!_mliScenery_colors_equal(a, b)) return 0;
+        if (!_mliScenery_surfaces_equal(a, b)) return 0;
+        if (!_mliScenery_triangles_equal(a, b)) return 0;
+        if (!_mliScenery_spherical_cap_hex_equal(a, b)) return 0;
+        if (!_mliScenery_spheres_equal(a, b)) return 0;
+        if (!_mliScenery_cylinders_equal(a, b)) return 0;
+        if (!_mliScenery_hexagons_equal(a, b)) return 0;
+        if (!_mliScenery_bicircleplanes_equal(a, b)) return 0;
+        if (!_mliScenery_discs_equal(a, b)) return 0;
         return 1;
 }
 
