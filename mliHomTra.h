04462043a2bfc0ffcc1678ff93ca65c1e847c98b
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-03-09 11:52:06 +0100

    lint, 8indent

--- mliHomTra.h
+++ mliHomTra.h
@@ -9,136 +9,172 @@
 #include "mliQuaternion.h"
 
 struct mliHomTraComp{
-    struct mliVec trans;
-    struct mliQuaternion rot;
+        struct mliVec trans;
+        struct mliQuaternion rot;
 };
 
 struct mliHomTra{
-    struct mliVec trans;
-    struct mliRotMat rot;
+        struct mliVec trans;
+        struct mliRotMat rot;
 };
 
-struct mliHomTra mliHomTra_from_compact(const struct mliHomTraComp trafo) {
-    struct mliHomTra t;
-    t.trans = trafo.trans;
-    t.rot = mliQuaternion_to_matrix(trafo.rot);
-    return t;}
+struct mliHomTra mliHomTra_from_compact(const struct mliHomTraComp trafo)
+{
+        struct mliHomTra t;
+        t.trans = trafo.trans;
+        t.rot = mliQuaternion_to_matrix(trafo.rot);
+        return t;
+}
 
 struct mliVec mli_transform_orientation(
-    const struct mliRotMat *rot,
-    const struct mliVec in) {
-    struct mliVec out;
-    out.x = in.x*rot->r00 + in.y*rot->r01 + in.z*rot->r02;
-    out.y = in.x*rot->r10 + in.y*rot->r11 + in.z*rot->r12;
-    out.z = in.x*rot->r20 + in.y*rot->r21 + in.z*rot->r22;
-    return out;}
+        const struct mliRotMat *rot,
+        const struct mliVec in)
+{
+        struct mliVec out;
+        out.x = in.x*rot->r00 + in.y*rot->r01 + in.z*rot->r02;
+        out.y = in.x*rot->r10 + in.y*rot->r11 + in.z*rot->r12;
+        out.z = in.x*rot->r20 + in.y*rot->r21 + in.z*rot->r22;
+        return out;
+}
 
 struct mliVec mli_transform_orientation_inverse(
-    const struct mliRotMat *rot,
-    const struct mliVec in) {
-    struct mliVec out;
-    out.x = in.x*rot->r00 + in.y*rot->r10 + in.z*rot->r20;
-    out.y = in.x*rot->r01 + in.y*rot->r11 + in.z*rot->r21;
-    out.z = in.x*rot->r02 + in.y*rot->r12 + in.z*rot->r22;
-    return out;}
+        const struct mliRotMat *rot,
+        const struct mliVec in)
+{
+        struct mliVec out;
+        out.x = in.x*rot->r00 + in.y*rot->r10 + in.z*rot->r20;
+        out.y = in.x*rot->r01 + in.y*rot->r11 + in.z*rot->r21;
+        out.z = in.x*rot->r02 + in.y*rot->r12 + in.z*rot->r22;
+        return out;
+}
 
 struct mliVec mli_transform_position(
-    const struct mliRotMat *rot,
-    const struct mliVec trans,
-    const struct mliVec in) {
-    struct mliVec out;
-    out.x = in.x*rot->r00 + in.y*rot->r01 + in.z*rot->r02 + trans.x;
-    out.y = in.x*rot->r10 + in.y*rot->r11 + in.z*rot->r12 + trans.y;
-    out.z = in.x*rot->r20 + in.y*rot->r21 + in.z*rot->r22 + trans.z;
-    return out;}
+        const struct mliRotMat *rot,
+        const struct mliVec trans,
+        const struct mliVec in)
+{
+        struct mliVec out;
+        out.x = in.x*rot->r00 + in.y*rot->r01 + in.z*rot->r02 + trans.x;
+        out.y = in.x*rot->r10 + in.y*rot->r11 + in.z*rot->r12 + trans.y;
+        out.z = in.x*rot->r20 + in.y*rot->r21 + in.z*rot->r22 + trans.z;
+        return out;
+}
 
 struct mliVec mli_transform_position_inverse(
-    const struct mliRotMat *rot,
-    const struct mliVec trans,
-    const struct mliVec in) {
-    struct mliVec out;
-    out.x = in.x*rot->r00 + in.y*rot->r10 + in.z*rot->r20 -
-        (rot->r00*trans.x + rot->r10*trans.y + rot->r20*trans.z);
-    out.y = in.x*rot->r01 + in.y*rot->r11 + in.z*rot->r21 -
-        (rot->r01*trans.x + rot->r11*trans.y + rot->r21*trans.z);
-    out.z = in.x*rot->r02 + in.y*rot->r12 + in.z*rot->r22 -
-        (rot->r02*trans.x + rot->r12*trans.y + rot->r22*trans.z);
-    return out;}
+        const struct mliRotMat *rot,
+        const struct mliVec trans,
+        const struct mliVec in)
+{
+        struct mliVec out;
+        out.x = in.x*rot->r00 + in.y*rot->r10 + in.z*rot->r20 -
+                (rot->r00*trans.x + rot->r10*trans.y + rot->r20*trans.z);
+        out.y = in.x*rot->r01 + in.y*rot->r11 + in.z*rot->r21 -
+                (rot->r01*trans.x + rot->r11*trans.y + rot->r21*trans.z);
+        out.z = in.x*rot->r02 + in.y*rot->r12 + in.z*rot->r22 -
+                (rot->r02*trans.x + rot->r12*trans.y + rot->r22*trans.z);
+        return out;
+}
 
 struct mliRay mli_transform_ray(
-    const struct mliRotMat *rot,
-    const struct mliVec trans,
-    const struct mliRay in) {
-    struct mliRay out;
-    out.support = mli_transform_position(
-        rot,
-        trans,
-        in.support);
-    out.direction = mli_transform_orientation(
-        rot,
-        in.direction);
-    return out;}
+        const struct mliRotMat *rot,
+        const struct mliVec trans,
+        const struct mliRay in)
+{
+        struct mliRay out;
+        out.support = mli_transform_position(
+                rot,
+                trans,
+                in.support);
+        out.direction = mli_transform_orientation(
+                rot,
+                in.direction);
+        return out;
+}
 
 struct mliRay mli_transform_ray_inverse(
-    const struct mliRotMat *rot,
-    const struct mliVec trans,
-    const struct mliRay in) {
-    struct mliRay out;
-    out.support = mli_transform_position_inverse(
-        rot,
-        trans,
-        in.support);
-    out.direction = mli_transform_orientation_inverse(
-        rot,
-        in.direction);
-    return out;}
+        const struct mliRotMat *rot,
+        const struct mliVec trans,
+        const struct mliRay in)
+{
+        struct mliRay out;
+        out.support = mli_transform_position_inverse(
+                rot,
+                trans,
+                in.support);
+        out.direction = mli_transform_orientation_inverse(
+                rot,
+                in.direction);
+        return out;
+}
 
-struct mliRay mliHomTra_ray(const struct mliHomTra *t, const struct mliRay in) {
-    return mli_transform_ray(&t->rot, t->trans, in);}
+struct mliRay mliHomTra_ray(
+    const struct mliHomTra *t,
+    const struct mliRay in)
+{
+        return mli_transform_ray(&t->rot, t->trans, in);
+}
 
-struct mliRay mliHomTra_ray_inverse(const struct mliHomTra *t, const struct mliRay in) {
-    return mli_transform_ray_inverse(&t->rot, t->trans, in);}
+struct mliRay mliHomTra_ray_inverse(
+        const struct mliHomTra *t,
+        const struct mliRay in)
+{
+        return mli_transform_ray_inverse(&t->rot, t->trans, in);
+}
 
-struct mliVec mliHomTra_pos(const struct mliHomTra *t, const struct mliVec in) {
-    return mli_transform_position(&t->rot, t->trans, in);}
+struct mliVec mliHomTra_pos(const struct mliHomTra *t, const struct mliVec in)
+{
+        return mli_transform_position(&t->rot, t->trans, in);
+}
 
 struct mliVec mliHomTra_pos_inverse(
-    const struct mliHomTra *t,
-    const struct mliVec in) {
-    return mli_transform_position_inverse(&t->rot, t->trans, in);}
+        const struct mliHomTra *t,
+        const struct mliVec in)
+{
+        return mli_transform_position_inverse(&t->rot, t->trans, in);
+}
 
-struct mliVec mliHomTra_dir(const struct mliHomTra *t, const struct mliVec in) {
-    return mli_transform_orientation(&t->rot, in);}
+struct mliVec mliHomTra_dir(const struct mliHomTra *t, const struct mliVec in)
+{
+        return mli_transform_orientation(&t->rot, in);
+}
 
 struct mliVec mliHomTra_dir_inverse(
-    const struct mliHomTra *t,
-    const struct mliVec in) {
-    return mli_transform_orientation_inverse(&t->rot, in);}
+        const struct mliHomTra *t,
+        const struct mliVec in)
+{
+        return mli_transform_orientation_inverse(&t->rot, in);
+}
 
-int mliHomTraComp_is_equal(const struct mliHomTraComp a, const struct mliHomTraComp b) {
-    if (!mliVec_is_equal(a.trans, b.trans))
-        return 0;
-    if (!mliQuaternion_is_equal(a.rot, b.rot))
-        return 0;
-    return 1;}
+int mliHomTraComp_is_equal(
+    const struct mliHomTraComp a,
+    const struct mliHomTraComp b)
+{
+        if (!mliVec_is_equal(a.trans, b.trans)) return 0;
+        if (!mliQuaternion_is_equal(a.rot, b.rot)) return 0;
+        return 1;
+}
 
 struct mliHomTraComp mliHomTraComp_sequence(
-    const struct mliHomTraComp a,
-    const struct mliHomTraComp b) {
-    struct mliHomTraComp s;
-    struct mliRotMat rot_a = mliQuaternion_to_matrix(a.rot);
-    s.trans = mli_transform_orientation(&rot_a, a.trans);
-    s.trans = mliVec_add(s.trans, b.trans);
-    s.rot = mliQuaternion_product(a.rot, b.rot);
-    return s;}
+        const struct mliHomTraComp a,
+        const struct mliHomTraComp b)
+{
+        struct mliHomTraComp s;
+        struct mliRotMat rot_a = mliQuaternion_to_matrix(a.rot);
+        s.trans = mli_transform_orientation(&rot_a, a.trans);
+        s.trans = mliVec_add(s.trans, b.trans);
+        s.rot = mliQuaternion_product(a.rot, b.rot);
+        return s;
+}
 
-void mliHomTra_print(const struct mliHomTra h) {
-    printf("[%.1f, %.1f, %.1f | %.1f]\n",
-        h.rot.r00, h.rot.r01, h.rot.r02, h.trans.x);
-    printf("[%.1f, %.1f, %.1f | %.1f]\n",
-        h.rot.r10, h.rot.r11, h.rot.r12, h.trans.y);
-    printf("[%.1f, %.1f, %.1f | %.1f]\n",
-        h.rot.r20, h.rot.r21, h.rot.r22, h.trans.z);
-    printf("[  0,   0,   0 |   1]\n");}
+void mliHomTra_print(const struct mliHomTra h)
+{
+        printf("[%.1f, %.1f, %.1f | %.1f]\n",
+                h.rot.r00, h.rot.r01, h.rot.r02, h.trans.x);
+        printf("[%.1f, %.1f, %.1f | %.1f]\n",
+                h.rot.r10, h.rot.r11, h.rot.r12, h.trans.y);
+        printf("[%.1f, %.1f, %.1f | %.1f]\n",
+                h.rot.r20, h.rot.r21, h.rot.r22, h.trans.z);
+        printf("[  0,   0,   0 |   1]\n");
+}
 
 #endif
--- mliRay.h
+++ mliRay.h
@@ -2,37 +2,43 @@
 #ifndef MERLICT_MLIRAY_H_
 #define MERLICT_MLIRAY_H_
 
+#include <stdio.h>
+
 struct mliRay{
-    struct mliVec support;
-    struct mliVec direction;
+        struct mliVec support;
+        struct mliVec direction;
 } mliRay;
 
 struct mliRay mliRay_set(
-    const struct mliVec support,
-    const struct mliVec direction) {
-    struct mliRay ray;
-    ray.support = support;
-    ray.direction = mliVec_multiply(direction, 1./mliVec_norm(direction));
-    return ray;}
+        const struct mliVec support,
+        const struct mliVec direction)
+{
+        struct mliRay ray;
+        ray.support = support;
+        ray.direction = mliVec_multiply(direction, 1./mliVec_norm(direction));
+        return ray;
+}
 
 struct mliVec mliRay_at(
-    const struct mliRay *ray,
-    const double t) {
-    struct mliVec out;
-    out.x = ray->support.x + t *ray->direction.x;
-    out.y = ray->support.y + t *ray->direction.y;
-    out.z = ray->support.z + t *ray->direction.z;
-    return out;}
+        const struct mliRay *ray,
+        const double t)
+{
+        struct mliVec out;
+        out.x = ray->support.x + t *ray->direction.x;
+        out.y = ray->support.y + t *ray->direction.y;
+        out.z = ray->support.z + t *ray->direction.z;
+        return out;
+}
 
-#include <stdio.h>
-
-void mliRay_print(const struct mliRay ray) {
-    printf("[%f, %f, %f] [%f, %f, %f]",
-        ray.support.x,
-        ray.support.y,
-        ray.support.z,
-        ray.direction.x,
-        ray.direction.y,
-        ray.direction.z);}
+void mliRay_print(const struct mliRay ray)
+{
+        printf("[%f, %f, %f] [%f, %f, %f]",
+                ray.support.x,
+                ray.support.y,
+                ray.support.z,
+                ray.direction.x,
+                ray.direction.y,
+                ray.direction.z);
+}
 
 #endif
