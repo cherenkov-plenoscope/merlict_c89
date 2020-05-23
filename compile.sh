d06de36827969a70cb4f434a1d7389b67d5355af
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-05-23 16:29:23 +0200

    respect 80 column limit

--- compile.sh
+++ compile.sh
@@ -3,26 +3,61 @@
 # At some point this should go to .travis.yml
 
 echo "gcc_c89"
-gcc merlict-c89-test.c -o test_gcc_c89 -std=c89 -lm -Wall -pedantic -Wstrict-prototypes
+gcc merlict-c89-test.c\
+        -o test_gcc_c89\
+        -std=c89\
+        -lm\
+        -Wall\
+        -pedantic\
+        -Wstrict-prototypes
+
 ./test_gcc_c89  > test_gcc_c89.o 2> test_gcc_c89.e
+
 tail -n 1 test_gcc_c89.o
 
 echo "gcc"
-gcc merlict-c89-test.c -o test_gcc -lm -Wall -pedantic -Wstrict-prototypes
+gcc merlict-c89-test.c\
+        -o test_gcc\
+        -lm\
+        -Wall\
+        -pedantic\
+        -Wstrict-prototypes
+
 ./test_gcc > test_gcc.o 2> test_gcc.e
+
 tail -n 1 test_gcc.o
 
 echo "g++"
-g++ merlict-c89-test.c -o test_g++ -lm -Wall -pedantic
+g++ merlict-c89-test.c\
+        -o test_g++\
+        -lm\
+        -Wall\
+        -pedantic
+
 ./test_g++ > test_g++.o 2> test_g++.e
+
 tail -n 1 test_g++.o
 
 echo "clang_c89"
-clang merlict-c89-test.c -o test_clang_c89 -std=c89 -lm -Wall -pedantic -Wstrict-prototypes
+clang merlict-c89-test.c\
+        -o test_clang_c89\
+        -std=c89\
+        -lm\
+        -Wall\
+        -pedantic\
+        -Wstrict-prototypes
+
 ./test_clang_c89 > test_clang_c89.o 2> test_clang_c89.e
+
 tail -n 1 test_clang_c89.o
 
 echo "clang"
-clang merlict-c89-test.c -o test_clang -lm -Wall -pedantic -Wstrict-prototypes
+clang merlict-c89-test.c\
+        -o test_clang\
+        -lm\
+        -Wall\
+        -pedantic
+
 ./test_clang > test_clang.o 2> test_clang.e
+
 tail -n 1 test_clang.o
