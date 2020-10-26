083fcf6ceb61a5e456f16454574c3541d0f0c4d7
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-10-26 10:00:36 +0100

    all obsolete with object-wavefronts

--- merlict_c89/mliHexagon.c
+++
@@ -1,7 +0,0 @@
-/* Copyright 2018-2020 Sebastian Achim Mueller */
-#include "mliHexagon.h"
-
-int mliHexagon_is_equal(const struct mliHexagon a, const struct mliHexagon b)
-{
-        return a.inner_radius == b.inner_radius;
-}
--- merlict_c89/mliScenery_read.c
+++ merlict_c89/mliScenery_read.c
@@ -1,153 +1,6 @@
 /* Copyright 2018-2020 Sebastian Achim Mueller */
 #include "mliScenery_read.h"
 
-int _mliScenery_read_vertices_and_triangles(struct mliScenery *scenery, FILE *f)
-{
-        mli_fread(
-                scenery->vertices,
-                sizeof(struct mliVec),
-                scenery->num_vertices,
-                f);
-        mli_fread(
-                scenery->triangles,
-                sizeof(struct mliFace),
-                scenery->num_triangles,
-                f);
-        mli_fread(
-                scenery->triangles_boundary_layers,
-                sizeof(struct mliBoundaryLayer),
-                scenery->num_triangles,
-                f);
-        return 1;
-error:
-        return 0;
-}
-
-int _mliScenery_read_spherical_cap_hex(struct mliScenery *scenery, FILE *f)
-{
-        mli_fread(
-                scenery->spherical_cap_hex,
-                sizeof(struct mliSphericalCapHex),
-                scenery->num_spherical_cap_hex,
-                f);
-        mli_fread(
-                scenery->spherical_cap_hex_boundary_layers,
-                sizeof(struct mliBoundaryLayer),
-                scenery->num_spherical_cap_hex,
-                f);
-        mli_fread(
-                scenery->spherical_cap_hex_T,
-                sizeof(struct mliHomTraComp),
-                scenery->num_spherical_cap_hex,
-                f);
-        return 1;
-error:
-        return 0;
-}
-
-int _mliScenery_read_spheres(struct mliScenery *scenery, FILE *f)
-{
-        mli_fread(scenery->spheres, sizeof(double), scenery->num_spheres, f);
-        mli_fread(
-                scenery->spheres_boundary_layers,
-                sizeof(struct mliBoundaryLayer),
-                scenery->num_spheres,
-                f);
-        mli_fread(
-                scenery->spheres_T,
-                sizeof(struct mliHomTraComp),
-                scenery->num_spheres,
-                f);
-        return 1;
-error:
-        return 0;
-}
-
-int _mliScenery_read_cylinders(struct mliScenery *scenery, FILE *f)
-{
-        mli_fread(
-                scenery->cylinders,
-                sizeof(struct mliCylinder),
-                scenery->num_cylinders,
-                f);
-        mli_fread(
-                scenery->cylinders_boundary_layers,
-                sizeof(struct mliBoundaryLayer),
-                scenery->num_cylinders,
-                f);
-        mli_fread(
-                scenery->cylinders_T,
-                sizeof(struct mliHomTraComp),
-                scenery->num_cylinders,
-                f);
-        return 1;
-error:
-        return 0;
-}
-
-int _mliScenery_read_hexagons(struct mliScenery *scenery, FILE *f)
-{
-        mli_fread(
-                scenery->hexagons,
-                sizeof(struct mliHexagon),
-                scenery->num_hexagons,
-                f);
-        mli_fread(
-                scenery->hexagons_boundary_layers,
-                sizeof(struct mliBoundaryLayer),
-                scenery->num_hexagons,
-                f);
-        mli_fread(
-                scenery->hexagons_T,
-                sizeof(struct mliHomTraComp),
-                scenery->num_hexagons,
-                f);
-        return 1;
-error:
-        return 0;
-}
-
-int _mliScenery_read_bicircleplanes(struct mliScenery *scenery, FILE *f)
-{
-        mli_fread(
-                scenery->bicircleplanes,
-                sizeof(struct mliBiCirclePlane),
-                scenery->num_bicircleplanes,
-                f);
-        mli_fread(
-                scenery->bicircleplanes_boundary_layers,
-                sizeof(struct mliBoundaryLayer),
-                scenery->num_bicircleplanes,
-                f);
-        mli_fread(
-                scenery->bicircleplanes_T,
-                sizeof(struct mliHomTraComp),
-                scenery->num_bicircleplanes,
-                f);
-        return 1;
-error:
-        return 0;
-}
-
-int _mliScenery_read_discs(struct mliScenery *scenery, FILE *f)
-{
-        mli_fread(
-                scenery->discs, sizeof(struct mliDisc), scenery->num_discs, f);
-        mli_fread(
-                scenery->discs_boundary_layers,
-                sizeof(struct mliBoundaryLayer),
-                scenery->num_discs,
-                f);
-        mli_fread(
-                scenery->discs_T,
-                sizeof(struct mliHomTraComp),
-                scenery->num_discs,
-                f);
-        return 1;
-error:
-        return 0;
-}
-
 int _mliScenery_read_user_ids(struct mliScenery *scenery, FILE *f)
 {
         mli_fread(
@@ -155,21 +8,6 @@ int _mliScenery_read_user_ids(struct mliScenery *scenery, FILE *f)
                 sizeof(uint32_t),
                 scenery->num_primitives,
                 f);
-        return 1;
-error:
-        return 0;
-}
-
-int mliScenery_read_capacity_from_file(struct mliSceneryCapacity *cap, FILE *f)
-{
-        mli_fread(&cap->num_vertices, sizeof(uint32_t), 1u, f);
-        mli_fread(&cap->num_triangles, sizeof(uint32_t), 1u, f);
-        mli_fread(&cap->num_spherical_cap_hex, sizeof(uint32_t), 1u, f);
-        mli_fread(&cap->num_spheres, sizeof(uint32_t), 1u, f);
-        mli_fread(&cap->num_cylinders, sizeof(uint32_t), 1u, f);
-        mli_fread(&cap->num_hexagons, sizeof(uint32_t), 1u, f);
-        mli_fread(&cap->num_bicircleplanes, sizeof(uint32_t), 1u, f);
-        mli_fread(&cap->num_discs, sizeof(uint32_t), 1u, f);
         return 1;
 error:
         return 0;
@@ -218,12 +56,6 @@ int mliScenery_read_from_path(struct mliScenery *scenery, const char *path)
         mli_fread(&scenery->default_medium, sizeof(uint32_t), 1u, f);
 
         mli_c(_mliScenery_read_vertices_and_triangles(scenery, f));
-        mli_c(_mliScenery_read_spherical_cap_hex(scenery, f));
-        mli_c(_mliScenery_read_spheres(scenery, f));
-        mli_c(_mliScenery_read_cylinders(scenery, f));
-        mli_c(_mliScenery_read_hexagons(scenery, f));
-        mli_c(_mliScenery_read_bicircleplanes(scenery, f));
-        mli_c(_mliScenery_read_discs(scenery, f));
 
         mli_c(_mliScenery_read_user_ids(scenery, f));
 
