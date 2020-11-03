80c506496e917c66f20a1a841f633930a4e76470
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-11-03 22:12:35 +0100

    expose string_split

--- merlict_c89/mli_string_to.h
+++ merlict_c89/mli_string_to.h
@@ -11,4 +11,9 @@
 int mli_string_ends_with(const char *str, const char *suffix);
 int mli_string_to_float(double *out, char *s);
 int mli_string_to_int(int64_t *out, char *s, uint64_t base);
+int mli_string_split(
+        const char *str,
+        const char delimiter,
+        char *token,
+        const uint64_t token_length);
 #endif
