4088cd0b871ed3bd61e47093f338df99801805a9
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-11-04 23:11:32 +0100

    get rid of crazy maps

--- merlict_c89/mliMap.h
+++ merlict_c89/mliMap.h
@@ -3,61 +3,18 @@
 #define MERLICT_C89_MLIMAP_H_
 
 #include <stdint.h>
-
 #include "mli_debug.h"
-#include "mli_variable_length_integer.h"
 
 #define MLI_MAP_KEY_CAPACITY 128
-#define MLI_MAP_TYPE_LEAF 1
-#define MLI_MAP_TYPE_NODE 2
 
-struct mliMap2 {
-        uint64_t capacity;
-        uint64_t size;
-        char **keys;
-        uint32_t *values;
-};
-
-struct mliMapNode {
-        struct mliMapNode *upper;
-        struct mliMapNode *lower;
-        uint64_t type;
+struct mliMap3Item {
+        char key[128];
         uint64_t value;
-        struct mliDynUint8 key;
 };
+MLIDYNARRAY_DEFINITON(mli, Map3, struct mliMap3Item)
+struct mliMap3Item mliMap3Item_init(void);
+int mliDynMap3_has(const struct mliDynMap3 *map, const char *key);
+int mliDynMap3_insert(struct mliDynMap3 *map, const char *key, uint64_t v);
+int mliDynMap3_get(const struct mliDynMap3 *map, const char *key, uint64_t *v);
 
-struct mliMap {
-        uint64_t size;
-        struct mliMapNode *root;
-};
-
-void mliMap_print(struct mliMap *map);
-void _mliMap_print(struct mliMapNode *node, const uint64_t indent);
-int mliMap_has(const struct mliMap *map, const char *key_str);
-int mliMap_get(const struct mliMap *map, const char *key_str, uint64_t *out);
-struct mliMapNode *_mliMap_has(
-        struct mliMapNode *node,
-        const struct mliDynUint8 *key);
-int mliMap_malloc_insert(
-        struct mliMap *map,
-        const char *key_str,
-        const uint64_t value);
-int _mliMap_insert(struct mliMapNode **node, struct mliMapNode *leaf);
-void mliMap_free(struct mliMap *map);
-void _mliMap_free_nodes(struct mliMapNode *node);
-struct mliMap mliMap_init(void);
-int mliMap2_has_value(const struct mliMap2 *map, const uint32_t value);
-int mliMap2_has(const struct mliMap2 *map, const char *key);
-int mliMap2_get_key(const struct mliMap2 *map, const uint32_t value, char *key);
-int mliMap2_get_value(
-        const struct mliMap2 *map,
-        const char *key,
-        uint32_t *value);
-int mliMap2_insert(struct mliMap2 *map, const char *key, const uint32_t value);
-int32_t _mliMap2_value_idx(const struct mliMap2 *map, const uint32_t value);
-int32_t _mliMap2_key_idx(const struct mliMap2 *map, const char *key);
-int mliMap2_malloc(struct mliMap2 *map, const uint64_t capacity);
-void _mliMap2_set_zero(struct mliMap2 *map);
-void mliMap2_free(struct mliMap2 *map);
-struct mliMap2 mliMap2_init(void);
 #endif
--- tests/test_map.c
+++ tests/test_map.c
@@ -58,54 +58,30 @@ CASE("vmap")
         mliDynMapItem_free(&map);
 }
 
-CASE("map")
+CASE("map3")
 {
         uint64_t ret = 0;
-        struct mliMap map = mliMap_init();
-        CHECK(mliMap_has(&map, "hans") == 0);
-        CHECK(mliMap_malloc_insert(&map, "hans", 1337));
-        CHECK(mliMap_has(&map, "hans"));
-        CHECK(mliMap_get(&map, "hans", &ret));
+        struct mliDynMap3 map = mliDynMap3_init();
+        CHECK(mliDynMap3_malloc(&map, 0u));
+
+        CHECK(mliDynMap3_has(&map, "hans") == 0);
+        CHECK(mliDynMap3_insert(&map, "hans", 1337));
+        CHECK(mliDynMap3_has(&map, "hans"));
+        CHECK(mliDynMap3_get(&map, "hans", &ret));
         CHECK(ret == 1337);
 
-        CHECK(mliMap_malloc_insert(&map, "peter", 42));
-        CHECK(mliMap_has(&map, "peter"));
-        CHECK(mliMap_get(&map, "peter", &ret));
+        CHECK(mliDynMap3_insert(&map, "peter", 42));
+        CHECK(mliDynMap3_has(&map, "peter"));
+        CHECK(mliDynMap3_get(&map, "peter", &ret));
         CHECK(ret == 42);
 
-        CHECK(mliMap_malloc_insert(&map, "karl", 37));
-        CHECK(mliMap_has(&map, "karl"));
-        CHECK(mliMap_get(&map, "karl", &ret));
+        CHECK(mliDynMap3_insert(&map, "karl", 37));
+        CHECK(mliDynMap3_has(&map, "karl"));
+        CHECK(mliDynMap3_get(&map, "karl", &ret));
         CHECK(ret == 37);
 
-        CHECK(mliMap_malloc_insert(&map, "karl", 37) == 1);
+        CHECK(!mliDynMap3_insert(&map, "karl", 37));
 
-        CHECK(map.size == 4u);
-        mliMap_free(&map);
-}
-
-CASE("map2")
-{
-        uint64_t i;
-        struct mliMT19937 prng = mliMT19937_init(0);
-        struct mliMap map = mliMap_init();
-
-        for (i = 0; i < 1000; i++) {
-                char key_str[1024];
-                uint64_t value = (uint64_t)(mli_random_uniform(&prng) * (1000));
-                sprintf(key_str, "%lu", value);
-
-                if (!mliMap_has(&map, key_str)) {
-                        CHECK(mliMap_malloc_insert(&map, key_str, value));
-                }
-        }
-
-        mliMap_free(&map);
-}
-
-CASE("empty_map")
-{
-        struct mliMap map = mliMap_init();
-        CHECK(map.size == 0u);
-        mliMap_free(&map);
+        CHECK(map.dyn.size == 3u);
+        mliDynMap3_free(&map);
 }
