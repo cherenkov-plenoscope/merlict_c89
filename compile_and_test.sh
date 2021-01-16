4130f0f2cad0a33da87232d4bcb80b0d49d2a8f6
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-01-16 12:46:35 +0100

    hide build and build-artifacts

--- compile_and_test.sh
+++ compile_and_test.sh
@@ -3,7 +3,7 @@
 
 echo "__gcc_c89__"
 gcc merlict-c89-test.c\
-        -o test_gcc_c89\
+        -o build/test_gcc_c89\
         -std=c89\
         -lm\
         -Wall\
@@ -11,39 +11,39 @@ gcc merlict-c89-test.c\
         -Wextra\
         -Wstrict-prototypes
 gcc_c89_rc=$?
-./test_gcc_c89 > test_gcc_c89.o 2> test_gcc_c89.e
+./build/test_gcc_c89 > ./build/test_gcc_c89.o 2> ./build/test_gcc_c89.e
 gcc_c89_test_rc=$?
 echo "compile" $gcc_c89_rc "test" $gcc_c89_test_rc
 
 echo "__gcc__"
 gcc merlict-c89-test.c\
-        -o test_gcc\
+        -o build/test_gcc\
         -lm\
         -Wall\
         -pedantic\
         -Wextra\
         -Wstrict-prototypes
 gcc_rc=$?
-./test_gcc > test_gcc.o 2> test_gcc.e
+./build/test_gcc > ./build/test_gcc.o 2> ./build/test_gcc.e
 gcc_test_rc=$?
 echo "compile" $gcc_rc "test" $gcc_test_rc
 
 echo "__g++__"
 g++ merlict-c89-test.c\
-        -o test_g++\
+        -o build/test_g++\
         -lm\
         -Wall\
         -pedantic\
         -Wextra
 gpp_rc=$?
-./test_g++ > test_g++.o 2> test_g++.e
+./build/test_g++ > ./build/test_g++.o 2> ./build/test_g++.e
 gpp_test_rc=$?
 echo "compile" $gpp_rc "test" $gpp_test_rc
 
 
 echo "__clang_c89__"
 clang merlict-c89-test.c\
-        -o test_clang_c89\
+        -o build/test_clang_c89\
         -std=c89\
         -lm\
         -Wall\
@@ -51,19 +51,19 @@ clang merlict-c89-test.c\
         -Wextra\
         -Wstrict-prototypes
 clang_c89_rc=$?
-./test_clang_c89 > test_clang_c89.o 2> test_clang_c89.e
+./build/test_clang_c89 > ./build/test_clang_c89.o 2> ./build/test_clang_c89.e
 clang_c89_test_rc=$?
 echo "compile" $clang_c89_rc "test" $clang_c89_test_rc
 
 echo "__clang__"
 clang merlict-c89-test.c\
-        -o test_clang\
+        -o build/test_clang\
         -lm\
         -Wall\
         -pedantic\
         -Wextra
 clang_rc=$?
-./test_clang > test_clang.o 2> test_clang.e
+./build/test_clang > ./build/test_clang.o 2> ./build/test_clang.e
 clang_test_rc=$?
 echo "compile" $clang_rc "test" $clang_test_rc
 
