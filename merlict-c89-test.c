632744a38c80f17f8c8ae19e73ba09a67856c45e
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-01-27 15:06:26 +0100

    obsolete

--- merlict-c89-test.c
+++ merlict-c89-test.c
@@ -29,7 +29,6 @@ int main(void)
 #include "tests/test_pseudo_random_number_generator.c"
 #include "tests/test_from_outside_to_inside.c"
 #include "tests/test_orientated_bounding_box.c"
-/*#include "tests/test_sphere_intersection_equation.c"*/
 #include "tests/test_mliUserScenery.c"
 #include "tests/test_scenery.c"
 #include "tests/test_mliDynArray.c"
