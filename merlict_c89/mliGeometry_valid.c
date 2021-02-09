eace7d8a824ca1204494966ba2024e0ca943df6b
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-02-09 14:23:49 +0100

    expose valid functions

--- merlict_c89/mliGeometry_valid.c
+++ merlict_c89/mliGeometry_valid.c
@@ -14,6 +14,27 @@ error:
         return 0;
 }
 
+int _mliGeometry_valid_robjects_HomTras(const struct mliGeometry *geometry)
+{
+        uint32_t i;
+        for (i = 0; i < geometry->num_robjects; i++) {
+                const struct mliVec t = geometry->robject2root[i].translation;
+                const struct mliQuaternion q = geometry->robject2root[i].rotation;
+                mli_check(!MLI_IS_NAN(t.x), "translation.x is 'nan'.");
+                mli_check(!MLI_IS_NAN(t.y), "translation.y is 'nan'.");
+                mli_check(!MLI_IS_NAN(t.z), "translation.z is 'nan'.");
+
+                mli_check(!MLI_IS_NAN(q.w), "quaternion.w is 'nan'.");
+                mli_check(!MLI_IS_NAN(q.x), "quaternion.x is 'nan'.");
+                mli_check(!MLI_IS_NAN(q.y), "quaternion.y is 'nan'.");
+                mli_check(!MLI_IS_NAN(q.z), "quaternion.z is 'nan'.");
+        }
+        return 1;
+error:
+        mli_log_err_vargs(("robject2root[%u] is invalid.", i));
+        return 0;
+}
+
 int _mliGeometry_valid_object_references(
         const struct mliGeometry *geometry,
         const struct mliMaterials *materials)
@@ -50,11 +71,6 @@ int _mliGeometry_valid_object_references(
                                 materials->num_surfaces,
                         "Expected object-reference to refer "
                         "to a valid outer medium.");
-
-                /*
-                 *       robject homogenous transformations can be whatever
-                 *       they want to be.
-                 */
         }
         return 1;
         mli_log_err_vargs(("robject[%u] is invalid.", i));
@@ -69,6 +85,9 @@ int mliGeometry_valid(
         mli_check(
                 _mliGeometry_valid_objects(geometry),
                 "Expected objects to be valid.");
+        mli_check(
+                _mliGeometry_valid_robjects_HomTras(geometry),
+                "Expected robject transformations to be free of 'nan'.");
         mli_check(
                 _mliGeometry_valid_object_references(geometry, materials),
                 "Expected object-references to be valid.");
