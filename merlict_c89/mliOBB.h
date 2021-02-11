79b701a3113508695981991c08399fd793286ed4
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-02-11 18:19:19 +0100

    obsolete edge, but keep drawing.

--- merlict_c89/mliOBB.h
+++ merlict_c89/mliOBB.h
@@ -13,13 +13,29 @@ struct mliOBB {
         /*
          * Rectangular Oriented-Bounding-Box
          * oriented w.r.t. the unit-vectors.
+         *
+         *                     O----------------------O
+         *                    /.                     /|
+         *                   / .                    / |
+         *                  /  .                   /  |
+         *                 /   .                  /   |
+         *                O----------------------O upper
+         *                |    .                 |    |
+         *      Z         |    .                 |    |
+         *      |       lo|wer O- - - - - - - - -| - -O
+         *      |         |   .                  |   /
+         *      |         |  .                   |  /
+         *      /-----Y   | .                    | /
+         *     /          |.                     |/
+         *    X           O----------------------O
+         *
+         *
          */
         struct mliVec lower;
         struct mliVec upper;
 };
 
 struct mliOBB mliOBB_set(const struct mliVec lower, const struct mliVec upper);
-struct mliEdge mliOBB_edge(const struct mliOBB obb, const uint64_t edge_idx);
 struct mliVec mliOBB_center(const struct mliOBB a);
 struct mliOBB mliOBB_outermost(const struct mliOBB a, const struct mliOBB b);
 int mliOBB_valid(const struct mliOBB obb);
