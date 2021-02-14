85395c77b28ea9ce8f64a4f9d79015b1400b9bb6
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-02-14 22:09:39 +0100

    put TU-Dortmund

--- merlict_c89/mli_version.c
+++ merlict_c89/mli_version.c
@@ -21,7 +21,7 @@ void mli_logo_fprint(FILE *f)
 void mli_authors_and_affiliations_fprint(FILE *f)
 {
         fprintf(f,
-                "  Sebastian Achim Mueller (1,2*)\n"
+                "  Sebastian Achim Mueller (1,2*,3^)\n"
                 "\n"
                 "  [1] Max-Planck-Institute for Nuclear Physics, \n"
                 "      Saupfercheckweg 1, 69117 Heidelberg, Germany\n"
@@ -29,5 +29,9 @@ void mli_authors_and_affiliations_fprint(FILE *f)
                 "  [2] Institute for Particle Physics and Astrophysics,\n"
                 "      ETH-Zurich, Otto-Stern-Weg 5, 8093 Zurich, Switzerland\n"
                 "\n"
-                "   *  (2015 - 2019)\n");
+                "  [3] Experimental Physics Vb, Astroparticle Physics,\n"
+                "      TU-Dortmund, Otto-Hahn-Str. 4a, 44227 Dortmund, Germany\n"
+                "\n"
+                "   *  (2015 - 2019)\n"
+                "   ^  (2013 - 2015)\n");
 }
