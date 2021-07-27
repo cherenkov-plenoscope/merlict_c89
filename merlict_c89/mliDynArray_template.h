1fb52bf262bb75cec5ab7a7d64fa40c50a4b2952
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-07-27 10:34:04 +0200

    update DynArray. No more casting from/to (void *), no more inner dyn struct.

--- merlict_c89/mliDynArray_template.h
+++ merlict_c89/mliDynArray_template.h
@@ -2,16 +2,15 @@
 #ifndef MERLICT_C89_MLIDYNARRAY_TEMPLATE_H_
 #define MERLICT_C89_MLIDYNARRAY_TEMPLATE_H_
 
-#include "mliDynArray.h"
+#include <stdint.h>
 #include "mliColor.h"
-#include "mliVec.h"
-#include "mliFace.h"
 
 #define MLIDYNARRAY_DEFINITON(LIB, NAME, PAYLOAD_TYPE)                         \
                                                                                \
         struct LIB##Dyn##NAME {                                                \
-                struct mliDynArray dyn;                                        \
-                PAYLOAD_TYPE *arr;                                             \
+                uint64_t capacity;                                             \
+                uint64_t size;                                                 \
+                PAYLOAD_TYPE *array;                                           \
         };                                                                     \
                                                                                \
         struct LIB##Dyn##NAME LIB##Dyn##NAME##_init(void);                     \
@@ -19,81 +18,111 @@
         void LIB##Dyn##NAME##_free(struct LIB##Dyn##NAME *dh);                 \
                                                                                \
         int LIB##Dyn##NAME##_malloc(                                           \
-                struct LIB##Dyn##NAME *dh, const uint64_t cap);                \
+                struct LIB##Dyn##NAME *dh,                                     \
+                const uint64_t size);                                          \
                                                                                \
         int LIB##Dyn##NAME##_push_back(                                        \
-                struct LIB##Dyn##NAME *dh, const PAYLOAD_TYPE pl);             \
-                                                                               \
-        int _##LIB##Dyn##NAME##_test_after_init(                               \
-                const struct LIB##Dyn##NAME *dh);                              \
-                                                                               \
-        int _##LIB##Dyn##NAME##_test_after_free(                               \
-                const struct LIB##Dyn##NAME *dh);                              \
-                                                                               \
-        int _##LIB##Dyn##NAME##_test_after_malloc(                             \
-                const struct LIB##Dyn##NAME *dh, uint64_t cap);
+                struct LIB##Dyn##NAME *dh,                                     \
+                PAYLOAD_TYPE item);
 
 #define MLIDYNARRAY_IMPLEMENTATION(LIB, NAME, PAYLOAD_TYPE)                    \
                                                                                \
         struct LIB##Dyn##NAME LIB##Dyn##NAME##_init(void)                      \
         {                                                                      \
                 struct LIB##Dyn##NAME dh;                                      \
-                mliDynArray_init_in_host(                                      \
-                        &dh.dyn, (void **)&dh.arr, sizeof(*dh.arr));           \
+                dh.capacity = 0u;                                              \
+                dh.size = 0u;                                                  \
+                dh.array = NULL;                                               \
                 return dh;                                                     \
         }                                                                      \
                                                                                \
         void LIB##Dyn##NAME##_free(struct LIB##Dyn##NAME *dh)                  \
         {                                                                      \
-                mliDynArray_free_in_host(&dh->dyn, (void **)&dh->arr);         \
+                free(dh->array);                                               \
+                (*dh) = LIB##Dyn##NAME##_init();                               \
         }                                                                      \
                                                                                \
         int LIB##Dyn##NAME##_malloc(                                           \
-                struct LIB##Dyn##NAME *dh, const uint64_t cap)                 \
+                struct LIB##Dyn##NAME *dh,                                     \
+                const uint64_t size)                                           \
         {                                                                      \
-                return mliDynArray_malloc_in_host(                             \
-                        &dh->dyn, (void **)&dh->arr, cap);                     \
+                LIB##Dyn##NAME##_free(dh);                                     \
+                dh->capacity = MLI_MAX2(2, size);                              \
+                dh->size = 0;                                                  \
+                mli_malloc(dh->array, PAYLOAD_TYPE, dh->capacity);             \
+                return 1;                                                      \
+        error:                                                                 \
+                return 0;                                                      \
         }                                                                      \
                                                                                \
         int LIB##Dyn##NAME##_push_back(                                        \
-                struct LIB##Dyn##NAME *dh, const PAYLOAD_TYPE pl)              \
+                struct LIB##Dyn##NAME *dh,                                     \
+                PAYLOAD_TYPE item)                                             \
         {                                                                      \
-                return mliDynArray_push_back_in_host(                          \
-                        &dh->dyn, (void **)&dh->arr, &pl);                     \
-        }                                                                      \
+                if (dh->size == dh->capacity) {                                \
+                        dh->capacity = dh->capacity * 2;                       \
+                        dh->array = realloc(                                   \
+                                dh->array,                                     \
+                                dh->capacity * sizeof(PAYLOAD_TYPE));          \
+                        mli_check_mem(dh->array);                              \
+                }                                                              \
                                                                                \
-        int _##LIB##Dyn##NAME##_test_after_init(                               \
-                const struct LIB##Dyn##NAME *dh)                               \
-        {                                                                      \
-                return _mliDynArray_test_after_init(                           \
-                        &dh->dyn, (const void **)&dh->arr, sizeof(*dh->arr));  \
-        }                                                                      \
+                dh->array[dh->size] = item;                                    \
+                dh->size += 1;                                                 \
                                                                                \
-        int _##LIB##Dyn##NAME##_test_after_free(                               \
-                const struct LIB##Dyn##NAME *dh)                               \
-        {                                                                      \
-                return _mliDynArray_test_after_free(                           \
-                        &dh->dyn, (const void **)&dh->arr, sizeof(*dh->arr));  \
-        }                                                                      \
-                                                                               \
-        int _##LIB##Dyn##NAME##_test_after_malloc(                             \
-                const struct LIB##Dyn##NAME *dh, uint64_t cap)                 \
-        {                                                                      \
-                return _mliDynArray_test_after_malloc(                         \
-                        &dh->dyn,                                              \
-                        (const void **)&dh->arr,                               \
-                        sizeof(*dh->arr),                                      \
-                        cap);                                                  \
+                return 1;                                                      \
+        error:                                                                 \
+                return 0;                                                      \
         }
 
+#define MLIDYNARRAY_TEST_DEFINITON(LIB, NAME, PAYLOAD_TYPE)                    \
+                                                                               \
+        int LIB##Dyn##NAME##_test_init(struct LIB##Dyn##NAME *dh);             \
+                                                                               \
+        int LIB##Dyn##NAME##_test_malloc(                                      \
+                struct LIB##Dyn##NAME *dh,                                     \
+                const uint64_t capacity);                                      \
+                                                                               \
+        int LIB##Dyn##NAME##_test_free(struct LIB##Dyn##NAME *dh);             \
+
+#define MLIDYNARRAY_TEST_IMPLEMENTATION(LIB, NAME, PAYLOAD_TYPE)               \
+                                                                               \
+        int LIB##Dyn##NAME##_test_init(struct LIB##Dyn##NAME *dh)              \
+        {                                                                      \
+                mli_c(dh->capacity == 0u);                                     \
+                mli_c(dh->size == 0u);                                         \
+                mli_c(dh->array == NULL);                                      \
+                return 1;                                                      \
+        error:                                                                 \
+                return 0;                                                      \
+        }                                                                      \
+                                                                               \
+        int LIB##Dyn##NAME##_test_malloc(                                      \
+                struct LIB##Dyn##NAME *dh,                                     \
+                const uint64_t capacity)                                       \
+        {                                                                      \
+                mli_c(dh->capacity >= dh->size);                               \
+                if (capacity < 2) {                                            \
+                        mli_c(dh->capacity == 2);                              \
+                } else {                                                       \
+                        mli_c(dh->capacity == capacity);                       \
+                }                                                              \
+                mli_c(dh->array != NULL);                                      \
+                return 1;                                                      \
+        error:                                                                 \
+                return 0;                                                      \
+        }                                                                      \
+                                                                               \
+        int LIB##Dyn##NAME##_test_free(struct LIB##Dyn##NAME *dh) {            \
+                return LIB##Dyn##NAME##_test_init(dh);                         \
+        }
+
+MLIDYNARRAY_DEFINITON(mli, Uint32, uint32_t)
 MLIDYNARRAY_DEFINITON(mli, Double, double)
+MLIDYNARRAY_DEFINITON(mli, Color, struct mliColor)
+MLIDYNARRAY_TEST_DEFINITON(mli, Color, struct mliColor)
+MLIDYNARRAY_DEFINITON(mli, ColorPtr, struct mliColor *)
 MLIDYNARRAY_DEFINITON(mli, Vec, struct mliVec)
 MLIDYNARRAY_DEFINITON(mli, Face, struct mliFace)
-MLIDYNARRAY_DEFINITON(mli, Color, struct mliColor)
-MLIDYNARRAY_DEFINITON(mli, ColorPtr, struct mliColor *)
-MLIDYNARRAY_DEFINITON(mli, Int64, int64_t)
-MLIDYNARRAY_DEFINITON(mli, Char, char)
-MLIDYNARRAY_DEFINITON(mli, Uint8, uint8_t)
-MLIDYNARRAY_DEFINITON(mli, Uint32, uint32_t)
 
 #endif
