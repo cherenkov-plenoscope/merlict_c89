d810bbeafaba69195792effa2f5b49d339663558
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-09-08 14:00:02 +0200

    just call it info

--- merlict_c89_corsika/mliTarIo.c
+++ merlict_c89_corsika/mliTarIo.c
@@ -13,20 +13,20 @@ struct mliTarIoRun mliTarIoRun_init(void)
 
 int mliTarIoRun_open(struct mliTarIoRun *run, const char *path)
 {
-        struct mliTarHeader tarinfo = mliTarHeader_init();
+        struct mliTarHeader info = mliTarHeader_init();
         mli_check(mliTar_open(&run->tar, path, "r"), "Failed to open tar.");
         mli_check(
-                mliTar_read_header(&run->tar, &tarinfo),
+                mliTar_read_header(&run->tar, &info),
                 "Failed to read tarinfo for corsika_run_header from tar.");
         mli_check(
-                strcmp(tarinfo.name, "runh.float32") == 0,
+                strcmp(info.name, "runh.float32") == 0,
                 "Expected first file to be 'runh.float32'.");
         mli_check(
-                tarinfo.size == 273 * sizeof(float),
+                info.size == 273 * sizeof(float),
                 "Expected corsika_run_header to have size 273*sizeof(float).");
         mli_check(
                 mliTar_read_data(
-                        &run->tar, (void *)&(run->corsika_run_header), tarinfo.size),
+                        &run->tar, (void *)&(run->corsika_run_header), info.size),
                 "Failed to read corsika_run_header from tar.");
         mli_check(
                 run->corsika_run_header[0] == mli_4chars_to_float("RUNH"),
