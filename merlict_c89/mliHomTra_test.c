cf60393d11b73c1e811adc95429d54918e2541b2
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-10-19 14:26:27 +0200

    includes

--- merlict_c89/mliHomTra_test.c
+++ merlict_c89/mliHomTra_test.c
@@ -1,5 +1,8 @@
 /* Copyright 2019-2020 Sebastian Achim Mueller                                */
 
+#include "mli_testing.h"
+#include "mliHomTra.h"
+
 CASE("sequence, unity and unity shall be unity")
 {
         struct mliHomTraComp a;
