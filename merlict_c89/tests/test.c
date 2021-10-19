2e29d356000677389207ee1968f555be92bde45c
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-10-19 10:03:37 +0200

    quadratic eq

--- merlict_c89/tests/test.c
+++ merlict_c89/tests/test.c
@@ -23,7 +23,7 @@ int main(void)
 #include "../../merlict_c89/mli_cstr_test.c"
 #include "../../merlict_c89/mli_cstr_and_numbers_test.c"
 #include "test_tape_archive.c"
-#include "test_quadratic_equation.c"
+#include "../../merlict_c89/mli_quadratic_equation_test.c"
 #include "test_pseudo_random_number_generator.c"
 #include "test_from_outside_to_inside.c"
 #include "test_orientated_bounding_box.c"
