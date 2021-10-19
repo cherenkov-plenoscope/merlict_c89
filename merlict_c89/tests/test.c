a2bf3b19470c6a8ff70f9458972132ac45c65f6d
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-10-19 16:10:04 +0200

    this is rather geometry than scenery

--- merlict_c89/tests/test.c
+++ merlict_c89/tests/test.c
@@ -54,7 +54,7 @@ int main(void)
 
 #include "../../merlict_c89/mliUserScenery_test.c"
 #include "../../merlict_c89/mliAccelerator_test.c"
-#include "../../merlict_c89/mliScenery_test.c"
+#include "../../merlict_c89/mliGeometry_test.c"
 #include "../../merlict_c89/mli_photon_propagation_test.c"
 #include "test_optics_prism.c"
 #include "test_optics_focussing_mirror.c"
