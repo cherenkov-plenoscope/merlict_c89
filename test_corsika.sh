fab7e1ee3d9e1221681c4e7fbe5b8e70a61b58ef
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2022-08-09 18:48:59 +0200

    adopt corsika changes

--- test_corsika.sh
+++ test_corsika.sh
@@ -1,12 +1,12 @@
-mv ./build/corsika/test_clang_c89 ./build/corsika/test_clang_c89.old
+mv ./build/corsika/test_clang_c99 ./build/corsika/test_clang_c99.old
 
-clang merlict_c89/mli_corsika_test.c\
-        -o build/corsika/test_clang_c89\
-        -std=c89\
+clang merlict/mli_corsika_test.c\
+        -o build/corsika/test_clang_c99\
+        -std=c99\
         -lm\
         -Wall\
         -pedantic\
         -Wextra\
         -Wstrict-prototypes
 
-./build/corsika/test_clang_c89
+./build/corsika/test_clang_c99
