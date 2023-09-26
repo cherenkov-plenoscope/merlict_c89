c5ba5b7850f6a211387fbd721088888f36d2b1bb
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-09-26 12:58:30 +0200

    mv json test resources

--- mli_core/tests/mli_json_test.c
+++ mli_core/tests/mli_json_test.c
@@ -77,9 +77,9 @@ CASE("mliJson_malloc_from_path")
         double myfloat;
 
         CHECK(mliJson_malloc_from_path(
-                &json, "tests/resources/mliJson/example.json"));
+                &json, "mli_core/tests/resources/mliJson/example.json"));
         CHECK(mliJson_debug_to_path(
-                &json, "tests/resources/mliJson/example.debug.tmp"));
+                &json, "mli_core/tests/resources/mliJson/example.debug.tmp"));
 
         CHECK(mliJson_token_by_key(&json, 0, "name", &return_idx));
         CHECK(return_idx == 1);
@@ -144,8 +144,10 @@ CASE("parse mliVec and mliColor")
         struct mliVec vec1 = mliVec_init(0., 0., 0.);
         struct mliVec vec2 = mliVec_init(0., 0., 0.);
         struct mliColor col = mliColor_set(0., 0., 0.);
-        CHECK(mliJson_malloc_from_path(&json, "tests/resources/vec.json"));
-        CHECK(mliJson_debug_to_path(&json, "tests/resources/vec.debug.tmp"));
+        CHECK(mliJson_malloc_from_path(
+                &json, "mli_core/tests/resources/mliJson/vec.json"));
+        CHECK(mliJson_debug_to_path(
+                &json, "mli_core/tests/resources/mliJson/vec.debug.tmp"));
 
         CHECK(mliJson_token_by_key(&json, 0, "vec1", &token));
         CHECK(mliVec_from_json_token(&vec1, &json, token + 1));
