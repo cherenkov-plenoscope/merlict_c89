4eb80af3a5cdb6e7427b86342332fdca826ef941
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2024-05-27 09:53:30 +0200

    obsolete

--- libs/mli_corsika/apps/ground_grid.main.c
+++ libs/mli_corsika/apps/ground_grid.main.c
@@ -10,189 +10,6 @@
 #include "../../mli/src/mliStr_numbers.h"
 #include "../../mli/src/mliDynArray.h"
 
-struct mliGroundGridPhotonBunch {
-        uint64_t index;
-        double weight;
-};
-
-MLIDYNARRAY_DEFINITON(
-        mli,
-        GroundGridPhotonBunch,
-        struct mliGroundGridPhotonBunch)
-
-MLIDYNARRAY_IMPLEMENTATION(
-        mli,
-        GroundGridPhotonBunch,
-        struct mliGroundGridPhotonBunch)
-
-struct mliGroundGrid {
-        struct mliDynGroundGridPhotonBunch *grid;
-        uint64_t num_x;
-        uint64_t num_y;
-        uint64_t num_overflow;
-};
-
-struct mliGroundGrid mliGroundGrid_init(void)
-{
-        struct mliGroundGrid gg;
-        gg.num_overflow = 0u;
-        gg.num_x = 0u;
-        gg.num_y = 0u;
-        gg.grid = NULL;
-        return gg;
-}
-
-void mliGroundGrid_free(struct mliGroundGrid *gg)
-{
-        if (gg->grid != NULL) {
-                uint64_t i;
-                for (i = 0; i < gg->num_x * gg->num_y; i++) {
-                        mliDynGroundGridPhotonBunch_free(&gg->grid[i]);
-                }
-        }
-        free(gg->grid);
-        (*gg) = mliGroundGrid_init();
-}
-
-int mliGroundGrid_malloc(
-        struct mliGroundGrid *gg,
-        const uint64_t num_x,
-        const uint64_t num_y)
-{
-        uint64_t i;
-        mliGroundGrid_free(gg);
-        gg->num_x = num_x;
-        gg->num_y = num_y;
-        chk_malloc(
-                gg->grid,
-                struct mliDynGroundGridPhotonBunch,
-                gg->num_x * gg->num_y);
-
-        for (i = 0; i < gg->num_x * gg->num_y; i++) {
-                gg->grid[i] = mliDynGroundGridPhotonBunch_init();
-                chk_msg(mliDynGroundGridPhotonBunch_malloc(&gg->grid[i], 0),
-                        "");
-        }
-
-        return 1;
-chk_error:
-        mliGroundGrid_free(gg);
-        return 0;
-}
-
-uint64_t mliGroundGrid_idx(
-        const struct mliGroundGrid *gg,
-        const uint64_t x,
-        const uint64_t y)
-{
-        return x * gg->num_y + y;
-}
-
-int mliGroundGrid_reset(struct mliGroundGrid *gg)
-{
-        uint64_t i;
-        gg->num_overflow = 0u;
-        for (i = 0; i < gg->num_x * gg->num_y; i++) {
-                if (gg->grid[i].size > 25) {
-                        chk(mliDynGroundGridPhotonBunch_malloc(
-                                &gg->grid[i], 25));
-                }
-                gg->grid[i].size = 0;
-        }
-
-        return 1;
-chk_error:
-        return 0;
-}
-
-int mliGroundGrid_assign(
-        struct mliGroundGrid *gg,
-        const uint64_t x,
-        const uint64_t y,
-        const uint64_t bunch_index,
-        const double bunch_weight)
-{
-        uint64_t i = mliGroundGrid_idx(gg, x, y);
-
-        struct mliGroundGridPhotonBunch bunch;
-        bunch.index = bunch_index;
-        bunch.weight = bunch_weight;
-
-        chk_msg(i < gg->num_x * gg->num_y, "index out of range.");
-        chk_msg(mliDynGroundGridPhotonBunch_push_back((&gg->grid[i]), bunch),
-                "woot?");
-
-        return 1;
-chk_error:
-        return 0;
-}
-
-int mliGroundGrid_assign_overlap(
-        struct mliGroundGrid *gg,
-        const struct mliDynUint32 *x_idxs,
-        const struct mliDynUint32 *y_idxs,
-        const uint64_t bunch_index,
-        const double bunch_weight)
-{
-        uint64_t oo;
-
-        if (x_idxs->size == 0) {
-                gg->num_overflow += 1;
-        } else {
-                for (oo = 0; oo < x_idxs->size; oo++) {
-                        chk(mliGroundGrid_assign(
-                                gg,
-                                x_idxs->array[oo],
-                                y_idxs->array[oo],
-                                bunch_index,
-                                bunch_weight));
-                }
-        }
-        return 1;
-chk_error:
-        return 0;
-}
-
-int mliGroundGrid_fprint_jsonl(const struct mliGroundGrid *gg, FILE *f)
-{
-        uint64_t xx, yy, ii, jj = 0;
-        uint64_t num_bins_written = 0;
-
-        chk(fprintf(f, "{"));
-        fprintf(f, "\"num_overflow\":%lu,", gg->num_overflow);
-        fprintf(f, "\"assignment\":{", gg->num_overflow);
-        for (xx = 0; xx < gg->num_x; xx++) {
-                for (yy = 0; yy < gg->num_y; yy++) {
-                        ii = mliGroundGrid_idx(gg, xx, yy);
-                        if (gg->grid[ii].size > 0) {
-                                if (num_bins_written > 0) {
-                                        fprintf(f, ",");
-                                }
-                                double bin_weight = 0.0;
-                                fprintf(f,
-                                        "\"%06lu,%06lu\":{\"idx\":[",
-                                        xx,
-                                        yy);
-                                for (jj = 0; jj < gg->grid[ii].size; jj++) {
-                                        struct mliGroundGridPhotonBunch bb =
-                                                gg->grid[ii].array[jj];
-                                        fprintf(f, "%lu", bb.index);
-                                        bin_weight += bb.weight;
-                                        if (jj + 1 < gg->grid[ii].size) {
-                                                fprintf(f, ",");
-                                        }
-                                }
-                                fprintf(f, "],\"weight\":%f}", bin_weight);
-                                num_bins_written = +1;
-                        }
-                }
-        }
-        fprintf(f, "}");
-        fprintf(f, "}\n");
-        return 1;
-chk_error:
-        return 0;
-}
 
 int read_config(
         const char *path,
@@ -264,7 +81,6 @@ int main(int argc, char *argv[])
         unsigned int num_bins_each_axis = 1000;
         uint64_t numover = (uint64_t)(1.5 * (double)num_bins_each_axis);
 
-        /*struct mliGroundGrid grid = mliGroundGrid_init();*/
         struct mliCorsikaHistogram2d hist = mliCorsikaHistogram2d_init();
 
         struct mliCorsikaPhotonBunch bunch;
@@ -298,14 +114,12 @@ int main(int argc, char *argv[])
         chk(mliEventTapeReader_begin(&arc, istream));
         chk(mliEventTapeReader_read_runh(&arc, runh));
 
-        /* chk(mliGroundGrid_malloc(&grid, x_bin_edges.size, y_bin_edges.size));*/
         chk(mliCorsikaHistogram2d_malloc(&hist, 10*1000));
         chk(mliIo_malloc_capacity(&buff, 10 * 1000));
 
         while (mliEventTapeReader_read_evth(&arc, evth)) {
                 uint64_t bunch_index = 0;
                 uint64_t xx, yy, ii, jj = 0;
-                /* mliGroundGrid_reset(&grid); */
 
                 if (hist.dict.capacity > 10 * 1000 * 1000) {
                         chk(mliCorsikaHistogram2d_malloc(&hist, 10*1000));
@@ -333,14 +147,6 @@ int main(int argc, char *argv[])
                                 &z_idxs,
                                 &overlaps);
 
-                        /*
-                        chk(mliGroundGrid_assign_overlap(
-                                &grid,
-                                &x_idxs,
-                                &y_idxs,
-                                bunch_index,
-                                bunch.weight_photons));
-                        */
                         for (oo = 0; oo < overlaps.size; oo ++)
                         {
                                 chk(
@@ -355,7 +161,6 @@ int main(int argc, char *argv[])
 
                         bunch_index += 1;
                 }
-                /*chk(mliGroundGrid_fprint_jsonl(&grid, ostream));*/
                 chk(mliCorsikaHistogram2d_dumps(&hist, &buff));
                 buff.pos = 0;
                 chk_fwrite(
@@ -365,7 +170,6 @@ int main(int argc, char *argv[])
                         ostream
                 );
         }
-        /* mliGroundGrid_free(&grid); */
         mliCorsikaHistogram2d_free(&hist);
 
         chk(mliEventTapeReader_finalize(&arc));
