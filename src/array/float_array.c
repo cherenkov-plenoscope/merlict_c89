6a173fbd1b03330e9354f9d26980de02761b7d3b
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2024-12-17 19:42:11 +0100

    split of primitive arrays

---
+++ src/array/float_array.c
@@ -0,0 +1,4 @@
+/* Copyright 2018-2020 Sebastian Achim Mueller */
+#include "float_array.h"
+
+MLI_ARRAY_IMPLEMENTATION(mli_ArrayFloat, float)
---
+++ src/array/float_array.h
@@ -0,0 +1,8 @@
+/* Copyright 2018-2020 Sebastian Achim Mueller */
+#ifndef MLIDYNARRAY_FLOAT_H_
+#define MLIDYNARRAY_FLOAT_H_
+
+#include "array.h"
+
+MLI_ARRAY_DEFINITON(mli_ArrayFloat, float)
+#endif
