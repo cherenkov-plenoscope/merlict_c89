0f21951d672e95a4f41d8b120de308e4140f0bf2
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2024-12-19 19:29:13 +0100

    add double array

---
+++ src/array/double_array.c
@@ -0,0 +1,3 @@
+/* Copyright 2018-2020 Sebastian Achim Mueller */
+#include "double_array.h"
+MLI_ARRAY_IMPLEMENTATION(mli_DoubleArray, double)
---
+++ src/array/double_array.h
@@ -0,0 +1,7 @@
+/* Copyright 2018-2020 Sebastian Achim Mueller */
+#ifndef MLI_DOUBLE_ARRAY_H_
+#define MLI_DOUBLE_ARRAY_H_
+
+#include "array.h"
+MLI_ARRAY_DEFINITON(mli_DoubleArray, double)
+#endif
