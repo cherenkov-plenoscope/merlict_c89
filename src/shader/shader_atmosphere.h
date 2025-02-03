f99a7894b2f784aa32478d748da65b6853bf84d2
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-02-03 12:14:18 +0100

    rm unused

--- src/shader/shader_atmosphere.h
+++ src/shader/shader_atmosphere.h
@@ -16,13 +16,5 @@ struct mli_ColorSpectrum mli_raytracing_color_tone_of_diffuse_sky(
         const struct mli_Shader *tracer,
         const struct mli_IntersectionSurfaceNormal *intersection,
         struct mli_Prng *prng);
-struct mli_ColorSpectrum mli_raytracing_to_intersection_atmosphere(
-        const struct mli_Shader *tracer,
-        const struct mli_IntersectionSurfaceNormal *intersection,
-        struct mli_Prng *prng);
-struct mli_ColorSpectrum mli_raytracing_with_atmosphere(
-        const struct mli_Shader *tracer,
-        const struct mli_Ray ray,
-        struct mli_Prng *prng);
 
 #endif
